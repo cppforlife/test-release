78e2a018ac1d9a9d2838b2c64784fa21820530ee
dmitriy kalinin <dkalinin@pivotal.io>
2016-03-02 17:37:01 -0800

    allow pre-start to fail

--- jobs/pre_start/templates/pre_start.sh
+++ jobs/pre_start/templates/pre_start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+
 echo <%= p("pre_start.stdout") %>
 echo <%= p("pre_start.stderr") %> 1>&2
 sleep <%= p("pre_start.delay_secs") %>
+
+if [ -f /tmp/pre-start-exit-code ]; then
+	exit `cat /tmp/pre-start-exit-code`
+fi
+
 exit <%= p("pre_start.exit_code") %>
